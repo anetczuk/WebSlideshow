040a332b90e184a89c24f7100b91b0c51d9f7e9f
anetczuk <anetczuk@o2.pl>
2017-03-09 23:25:05 +0100

    Remove old test cases

--- provider/NasaTest.py
+++ provider/NasaTest.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #
 # <one line to give the library's name and an idea of what it does.>
 # Copyright (C) 2017  Bob <email>
@@ -29,39 +31,6 @@ class NasaTest(unittest.TestCase):
     def tearDown(self):
         # Called after the last testfunction was executed
         pass
-
-
-    def test_parse001(self):
-        body = """
-                <div class="object">
-                        <div class="podpis">
-                                <span class="lewa">
-                                        komentarze (<fb:comments-count href=http://pokazcyckidamciciastko.pl/cycki/2007/mmm-lt3/></fb:comments-count>)
-                                        
-                                </span>
-                                <span class="prawa">
-                                        <a href="#" class="thumb_up" onClick="vote_up(2007); return false;">dobre</a>
-                                        <span style="color:#fff;" class="rate_2007">?%</span>
-                                        <a href="#" class="thumb_down" onClick="vote_down(2007); return false;">słabe</a>
-                                </span>
-                        </div>
-                        <a href="#"><img src="http://pokazcyckidamciciastko.pl/upload/20121112112157.png" title="mmm... &lt;3" alt="mmm... &lt;3" /></a>
-                </div>
-""";
-        provider = Ciastko()
-        links = provider.parsePage(body)        
-        self.assertEqual(links, ['http://pokazcyckidamciciastko.pl/upload/20121112112157.png'])
-
-    def test_random(self):
-        provider = Ciastko()
-        url = provider.randomUrl()
-        self.assertGreater(len(url), 0)
-        
-    def test_provide(self):
-        provider = Ciastko()
-        url = provider.provideUrl()
-        ## print "Links:\n", provider.links
-        self.assertGreater( len(url), 0 )
 
 
 if __name__ == "__main__":
