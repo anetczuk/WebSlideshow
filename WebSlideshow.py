40478ad117d3b26d5e40492520791359c8ff2d58
anetczuk <anetczuk@o2.pl>
2017-03-09 23:34:00 +0100

    Remove bad imports

--- WebSlideshow.py
+++ WebSlideshow.py
@@ -13,8 +13,6 @@ import argparse
 
 ##from provider.UrlProvider import ExampleProvider
 from provider.Demotywatory import Demotywatory
-from provider.Ciastko import Ciastko
-from provider.Gag9 import Gag9
 from provider.Break import Break
 from provider.Nasa import Nasa
 
@@ -44,9 +42,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Display internet gallery.')
     parser.add_argument('--Demoty', action='store_const', const=True, default=True, help='Use www.demotywatory.pl gallery (default)' )
     parser.add_argument('--Nasa', action='store_const', const=True, help='Use https://apod.nasa.gov/apod/random_apod.html gallery' )
-    parser.add_argument('--Gag9', action='store_const', const=True, help='Use www.9gag.com gallery' )
     parser.add_argument('--Break', action='store_const', const=True, help='Use www.break.com/pictures gallery' )
-    parser.add_argument('--Ciastka', action='store_const', const=True, help='Use gallery for cookie' )
     parser.add_argument('--random', action='store_const', const=True, default=False, help="Gallery random mode (if supporting)" )
     parser.add_argument('--dtime', action='store', default=10, help="Display time of single image [s]" )
     parser.add_argument('--nofs', action='store_const', const=True, default=False, help='No fullscreen at startup' )
